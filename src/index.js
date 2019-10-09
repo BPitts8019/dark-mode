568c3021c4f12a3975da74aee1720163794f0217
BPitts8019 <bradleyj.pitts@gmail.com>
2019-10-08 18:47:32 -0700

    Data Filter works

--- src/index.js
+++ src/index.js
@@ -10,7 +10,9 @@ import "./styles.scss";
 
 const App = () => {
    const [coinData, setCoinData] = useState([]);
+   const [filteredData, setFilteredData] = useState([]);
    const [dataFilter, setDataFilter] = useState("all");
+
    const filterProps = {
       coinData,
       dataFilter,
@@ -22,19 +24,34 @@ const App = () => {
          .get(
             "https://api.coingecko.com/api/v3/coins/markets?vs_currency=usd&order=market_cap_desc&per_page=10&page=1&sparkline=true"
          )
-         .then(res => setCoinData(res.data))
+         .then(res => {
+            setCoinData(res.data);
+            setFilteredData(res.data);
+         })
          .catch(err => console.log(err));
    }, []);
 
    useEffect(() => {
-      console.log(`Data Filter changed to: '${dataFilter}'`)
+      console.log(`Data Filter changed to: '${dataFilter}'`);
+      let newData = [];
+      
+      if (coinData.length > 0) {
+         if (dataFilter === "all") {
+            newData = [...coinData];
+         } else {
+            newData = coinData.filter(coin => coin.symbol === dataFilter);
+         }
+   
+         // console.log(`New Data: `, JSON.stringify(newData, null, 3));
+         setFilteredData(newData);
+      }
    }, [dataFilter]);
 
    return (
       <div className="App">
          <Navbar />
          <Toolbar {...filterProps} />
-         <Charts coinData={coinData} />
+         <Charts coinData={filteredData} />
       </div>
    );
 };
